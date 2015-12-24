efdd43449793dd5dc1fd9ca79d6844dbae964f6c
Will Bradley <bradley.will@gmail.com>
2015-12-24 14:10:48 -0700

    Changing bucket name and region

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -8,8 +8,8 @@ if Rails.env.production? || Rails.env.staging?
   aws = {
     access_key_id: ENV["AWS_ACCESS_KEY"],
     secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-    region: "sa-east-1",
-    bucket: "my-bucket",
+    region: "us-west-1",
+    bucket: "dhpa-uploads",
   }
   Refile.cache = Refile::S3.new(prefix: "cache", **aws)
   Refile.store = Refile::S3.new(prefix: "store", **aws)
