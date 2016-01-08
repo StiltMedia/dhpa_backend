6d175592bb8e47bfca12820c8d75ac74575e35a4
Will Bradley <bradley.will@gmail.com>
2016-01-08 13:24:08 -0700

    Improving display of search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,13 +3,18 @@ class SearchController < ApplicationController
   def search
     @events = Event.fuzzy_search(params[:q])
     @events = (@events + Event.tagged_with(params[:q], wild: true)).uniq
+    @events = @events.sort_by(&:date).reverse
+
     @photos = Photo.fuzzy_search(params[:q])
     @photos = (@photos + Photo.tagged_with(params[:q], wild: true)).uniq
-    @vips = Vip.fuzzy_search(params[:q])
+    @photos = @photos.sort_by(&:created_at).reverse
+
+    @vips = Vip.fuzzy_search(params[:q]).sort_by(&:name)
   end
 
   def vip
     @vip = Vip.find(params[:id])
+    @vip_photos = @vip.photos.joins(:event).where("events.password IS NULL").order(created_at: :desc)
   end
 
 end
