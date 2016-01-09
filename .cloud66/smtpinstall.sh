3ec2a6c25a8c4b7455c6b45b1df93f923ec1cc48
Will Bradley <bradley.will@gmail.com>
2016-01-09 13:18:57 -0700

    Updating smtp script

--- .cloud66/smtpinstall.sh
+++ .cloud66/smtpinstall.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # {{Description: This deploy hook will install and configure Postfix. Set the SMTP_HOSTNAME environment variable to configure the domain name. }}
-sudo apt-get update -y
-sudo apt-get install postfix -y
-
 if [ -z "$SMTP_HOSTNAME" ]
 then
-    >&2 echo "Please set your SMTP_HOSTNAME environment variable; left it as the default."
-    export SMTP_HOSTNAME="smtp.example.com"
-fi
+    >&2 echo "Please set your SMTP_HOSTNAME environment variable and run again."
+else
+  debconf-set-selections <<< "postfix postfix/mailname string $SMTP_HOSTNAME"
+  debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
+
+  sudo apt-get update -y
+  sudo apt-get install postfix -y
 
 # Important that there are no leading spaces in this echo
 echo "myhostname = $SMTP_HOSTNAME
@@ -16,3 +17,5 @@ relayhost = \$mydomain
 inet_interfaces = loopback-only
 mydestination =
 " > /etc/postfix/main.cf
+
+fi
